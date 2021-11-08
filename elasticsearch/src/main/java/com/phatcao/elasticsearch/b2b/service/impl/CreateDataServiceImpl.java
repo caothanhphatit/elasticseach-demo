1de7d0b5ab8457dda5f5c123d80dc93579aa6852
phat.cao@digicommercegroup.com <phat.cao@digicommercegroup.com>
2021-11-08 21:55:20 +0700

    push some code

--- elasticsearch/src/main/java/com/phatcao/elasticsearch/b2b/service/impl/CreateDataServiceImpl.java
+++ elasticsearch/src/main/java/com/phatcao/elasticsearch/b2b/service/impl/CreateDataServiceImpl.java
@@ -9,6 +9,7 @@ import com.phatcao.elasticsearch.b2b.dao.repository.ProductRepository;
 import com.phatcao.elasticsearch.b2b.dto.response.ResponseData;
 import com.phatcao.elasticsearch.b2b.service.CreateDataService;
 import com.phatcao.elasticsearch.b2b.utils.converter.ESProductConverter;
+import com.phatcao.elasticsearch.elasticsearch.dao.document.ESProductEntity;
 import com.phatcao.elasticsearch.elasticsearch.dto.indexing.ESProduct;
 import com.phatcao.elasticsearch.elasticsearch.service.ESCreateDataService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,9 +47,9 @@ public class CreateDataServiceImpl implements CreateDataService
 
 		List<CategoryEntity> categoryEntities = categoryRepository.findAllByIsDeleted(false);
 
-		final List<ESProduct> products = esProductConverter.convertToESProduct(productEntities, categoryEntities, customerEntities);
+		final List<ESProductEntity> products = esProductConverter.convertToESProduct(productEntities, categoryEntities, customerEntities);
 
-		boolean success = createDataService.createData(products);
+//		boolean success = createDataService.createData(products);
 
 
 //		responseData.setData(productEntityList);
--- elasticsearch/src/main/java/com/phatcao/elasticsearch/b2b/utils/converter/impl/ESProductConverterImpl.java
+++ elasticsearch/src/main/java/com/phatcao/elasticsearch/b2b/utils/converter/impl/ESProductConverterImpl.java
@@ -4,9 +4,10 @@ import com.phatcao.elasticsearch.b2b.dao.entity.CategoryEntity;
 import com.phatcao.elasticsearch.b2b.dao.entity.CustomerEntity;
 import com.phatcao.elasticsearch.b2b.dao.entity.ProductEntity;
 import com.phatcao.elasticsearch.b2b.utils.converter.ESProductConverter;
-import com.phatcao.elasticsearch.elasticsearch.dto.indexing.ESProduct;
+import com.phatcao.elasticsearch.elasticsearch.dao.document.ESProductEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,9 +15,50 @@ import java.util.List;
 public class ESProductConverterImpl implements ESProductConverter
 {
 	@Override
-	public List<ESProduct> convertToESProduct(final List<ProductEntity> productEntities,
+	public List<ESProductEntity> convertToESProduct(final List<ProductEntity> productEntities,
 			final List<CategoryEntity> categoryEntities, final List<CustomerEntity> customerEntities)
 	{
-		return null;
+		List<ESProductEntity> result = new ArrayList<>();
+
+		productEntities.forEach(productEntity -> {
+
+			ESProductEntity esProductEntity = convertToESProduct(productEntity);
+
+			categoryEntities.forEach(categoryEntity -> {
+
+
+
+
+			});
+
+
+
+
+
+		});
+
+		return result;
 	}
+
+
+	ESProductEntity convertToESProduct(ProductEntity productEntity){
+
+			ESProductEntity esProductEntity = new ESProductEntity();
+
+			esProductEntity.setProductUuid(productEntity.getProductUuid());
+			esProductEntity.setDiscount (productEntity.getDiscount());
+			esProductEntity.setProductName(productEntity.getProductName());
+			esProductEntity.setProductPrice(productEntity.getProductPrice());
+			esProductEntity.setProductDescription(productEntity.getProductDescription());
+			esProductEntity.setProductImage(productEntity.getProductImageUrl());
+			esProductEntity.setProductSKU(productEntity.getSku());
+			esProductEntity.setBaseProduct(productEntity.isBaseProduct());
+			esProductEntity.setBaseProductId(productEntity.getProductBaseId());
+			esProductEntity.setProductType(productEntity.getProductType());
+			esProductEntity.setBaseProductId(productEntity.getProductBaseId());
+
+			return esProductEntity;
+
+	}
+
 }
